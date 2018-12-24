eb1e66b70a9c7880eb9e3ac7fb938782a6de16f0
welldoer <welldoer@263.net>
2018-12-24 23:13:18 +0800

    准备Gradle环境。

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # OpenProj-TDD
+用TDD方法来尝试重新开发一版 [OpenProj](https://sourceforge.net/projects/openproj/)
+
+#### 待开发任务清单
+~~1. 在GitHub中新建repo~~
+~~2. 在本地Eclipse开发环境Clone，并加载Gradle环境~~
+3. 添加Gradle相关配置，并编写第一个测试案例
+4. 配置Travis-CI
+5. 配置SonarCloud
---
+++ src/test/java/net/blogjava/openproj/TestMain.java
@@ -0,0 +1,24 @@
+package net.blogjava.openproj;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestMain {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		assertThat( true ).isEqualTo( true );
+	}
+
+}
